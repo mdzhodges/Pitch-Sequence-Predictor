2dcbd2f801235c4cc98cb0e8e0aa0288956b133c
Henry Rothenberg <hsr205@aol.com>
2025-11-11 11:52:27 -0500

    Add field to constants.py module

--- utils/constants.py
+++ utils/constants.py
@@ -16,3 +16,30 @@ class Constants:
         "plate_x", "plate_z",
         "events"
     ]
+
+    CONTEXT_COLUMNS_LIST:list[str] = [
+        'age_bat', 'age_bat_legacy', 'age_pit', 'age_pit_legacy',
+        'api_break_x_arm', 'api_break_x_batter_in', 'api_break_z_with_gravity',
+        'arm_angle', 'at_bat_number', 'attack_angle', 'attack_direction',
+        'away_score', 'ax', 'ay', 'az', 'babip_value', 'balls', 'bat_score',
+        'bat_score_diff', 'bat_speed', 'bat_win_exp', 'batter', 'bb_type',
+        'break_angle_deprecated', 'break_length_deprecated',
+        'delta_home_win_exp', 'delta_pitcher_run_exp', 'delta_run_exp', 'des',
+        'effective_speed', 'estimated_ba_using_speedangle',
+        'estimated_slg_using_speedangle', 'estimated_woba_using_speedangle',
+        'events', 'fld_score', 'hc_x', 'hc_y', 'hit_distance_sc',
+        'hit_location', 'home_score', 'home_score_diff', 'home_team',
+        'home_win_exp', 'hyper_speed', 'if_fielding_alignment', 'inning',
+        'inning_topbot', 'intercept_ball_minus_batter_pos_x_inches',
+        'intercept_ball_minus_batter_pos_y_inches', 'iso_value', 'launch_angle',
+        'launch_speed', 'launch_speed_angle', 'n_priorpa_thisgame_player_at_bat',
+        'n_thruorder_pitcher', 'of_fielding_alignment', 'on_1b', 'on_2b', 'on_3b',
+        'outs_when_up', 'p_throws', 'pfx_x', 'pfx_z', 'pitch_name',
+        'pitch_number', 'pitch_type', 'pitcher', 'pitcher_days_since_prev_game',
+        'pitcher_days_until_next_game', 'plate_x', 'plate_z', 'player_name',
+        'post_away_score', 'post_bat_score', 'post_fld_score', 'post_home_score',
+        'release_extension', 'release_pos_x', 'release_pos_y', 'release_pos_z',
+        'release_speed', 'release_spin_rate', 'spin_axis', 'spin_dir', 'stand',
+        'strikes', 'sv_id', 'swing_length', 'swing_path_tilt', 'sz_bot', 'sz_top',
+        'type', 'vx0', 'vy0', 'vz0', 'woba_denom', 'woba_value', 'zone'
+    ]
