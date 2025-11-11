9b7938a06f7611d270add30d4f5e1b5b07ee171e
Henry Rothenberg <hsr205@aol.com>
2025-11-11 10:13:17 -0500

    Alter data export logic in pitcher_collection.py module

--- data_collection/pitcher_collection.py
+++ data_collection/pitcher_collection.py
@@ -9,10 +9,10 @@ from utils.logger import Logger
 class PitcherDataCollection:
 
     def __init__(self) -> None:
-        self.export_file_path: Path = Path("data/pitchers_2025_full.csv")
+        self.export_file_path: Path = Path("data/pitchers_2025_full.parquet")
         self.logger = Logger(self.__class__.__name__)
 
-    def get_pitcher_stats_dataframe(self) -> pd.DataFrame:
+    def _get_pitcher_stats_dataframe(self) -> pd.DataFrame:
 
         try:
 
@@ -36,18 +36,18 @@ class PitcherDataCollection:
             self.logger.error(f"Exception thrown while retrieving pitching data: {e}")
             raise Exception(f"Exception thrown while retrieving pitching data: {e}")
 
-    def export_pitcher_data_to_csv(self) -> None:
+    def export_pitcher_data_to_parquet_file(self) -> None:
 
-        pitcher_stats_dataframe: pd.DataFrame = self.get_pitcher_stats_dataframe()
+        pitcher_stats_dataframe: pd.DataFrame = self._get_pitcher_stats_dataframe()
 
         try:
 
-            self.logger.info("Exporting pitcher data:")
+            self.logger.info(f"Exporting pitcher data to: {self.export_file_path}")
             self.logger.info("=" * 100)
 
-            pitcher_stats_dataframe.to_csv(path_or_buf=self.export_file_path, encoding="utf-8", index=False)
+            pitcher_stats_dataframe.to_parquet(path=self.export_file_path, index=False)
 
-            self.logger.info("Successfully exported pitcher data:")
+            self.logger.info(f"Successfully exported pitcher data to: {self.export_file_path}")
             self.logger.info("=" * 100)
 
         except Exception as e:
