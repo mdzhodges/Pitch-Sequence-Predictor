e25c3ed8f70fa18988cf858dfda943e66bd0a3d9
Henry Rothenberg <hsr205@aol.com>
2025-11-11 09:46:56 -0500

    Refactor pitcher_collection.py module

--- data_collection/pitcher_collection.py
+++ data_collection/pitcher_collection.py
@@ -1,14 +1,55 @@
-from pybaseball import pitching_stats
+from pathlib import Path
+
 import pandas as pd
+from pybaseball import pitching_stats
 
-# Get every single stat possible from every hitter possiible
-pitching_stats = pitching_stats(2025, qual=0)
+from utils.logger import Logger
 
-# Remove hitters with no PA
-pitching_stats = pitching_stats[pitching_stats["IP"] > 0].reset_index(drop=True)
 
-# Save everything
-pitching_stats.to_csv("data/pitchers_2025_full.csv", index=False)
+class PitcherDataCollection:
 
-print(pitching_stats.head(5))
-print("Retrieved")
+    def __init__(self) -> None:
+        self.export_file_path: Path = Path("data/pitchers_2025_full.csv")
+        self.logger = Logger(self.__class__.__name__)
+
+    def get_pitcher_stats_dataframe(self) -> pd.DataFrame:
+
+        try:
+
+            self.logger.info("Retrieving pitcher data:")
+            self.logger.info("=" * 100)
+
+            # Retrieve data from the 2025 MLB season
+            pitcher_stats_dataframe: pd.DataFrame = pitching_stats(2025, qual=0)
+
+            # Remove pitchers with no innings pitched
+            result_dataframe: pd.DataFrame = pitcher_stats_dataframe[pitcher_stats_dataframe["IP"] > 0].reset_index(
+                drop=True)
+
+            self.logger.info("Successfully retrieved pitcher data:")
+            self.logger.info("=" * 100)
+
+            return result_dataframe
+
+
+        except Exception as e:
+            self.logger.error(f"Exception thrown while retrieving pitching data: {e}")
+            raise Exception(f"Exception thrown while retrieving pitching data: {e}")
+
+    def export_pitcher_data_to_csv(self) -> None:
+
+        pitcher_stats_dataframe: pd.DataFrame = self.get_pitcher_stats_dataframe()
+
+        try:
+
+            self.logger.info("Exporting pitcher data:")
+            self.logger.info("=" * 100)
+
+            pitcher_stats_dataframe.to_csv(path_or_buf=self.export_file_path, encoding="utf-8", index=False)
+
+            self.logger.info("Successfully exported pitcher data:")
+            self.logger.info("=" * 100)
+
+        except Exception as e:
+            self.logger.error(f"Exception thrown while exporting pitching data: {e}")
+            raise Exception(f"Exception thrown while exporting pitching data: {e}")
