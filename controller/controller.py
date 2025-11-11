354b842543f2b4d0cf5b498f7c8ced1e8942ad7d
Henry Rothenberg <hsr205@aol.com>
2025-11-11 10:22:00 -0500

    Add parsed_args as instance member in controller.py module

--- controller/controller.py
+++ controller/controller.py
@@ -1,11 +1,11 @@
-from utils.logger import Logger
-from model.pitch_sequence_pipeline import PitchSequencePipeline
 from controller.cli_arguments import CLIArguments
+from model.pitch_sequence_pipeline import PitchSequencePipeline
+from utils.logger import Logger
 
 
 class Controller:
 
     def __init__(self, parsed_args: CLIArguments) -> None:
+        self.parsed_args = parsed_args
         self._logger = Logger(self.__class__.__name__)
         PitchSequencePipeline()
-        
