8e9cff92eec9811860bb32309ca849cfdee1eb40
Henry Rothenberg <hsr205@aol.com>
2025-11-12 13:30:39 -0500

    Remove unused arguments in pitch_sequence_pipeline.py module

--- model/pitch_sequence_pipeline.py
+++ model/pitch_sequence_pipeline.py
@@ -1,35 +1,38 @@
+from model.data_types import ModelComponents
 from model.pitch_sequence_encoder.pitch_sequence_encoder import PitchSequenceEncoder
 from model.pitch_sequence_pipeline_components import PitchSequencePipelineComponents
+from model.trainer_type import TrainerComponents
 from model.training.pitch_sequence_trainer import PitchSequenceTrainer
 from preprocessing.context_dataset import ContextDataset
 from preprocessing.hitter_dataset import HitterDataset
 from preprocessing.pitch_sequence_dataset import PitchSequenceDataset
 from preprocessing.pitcher_dataset import PitcherDataset
 from utils.logger import Logger
-from model.data_types import ModelComponents
-from model.trainer_type import TrainerComponents
+
 
 class PitchSequencePipeline:
 
     def __init__(self, pitch_sequence_pipeline_components: PitchSequencePipelineComponents):
         self.logger = Logger(self.__class__.__name__)
-        
-        #Sample for the pitch sequence dataset
+
+        # Sample for the pitch sequence dataset
         self.sample = pitch_sequence_pipeline_components.sample
-        
-        
+
         # Get data tensors
         self.hitter_dataset = HitterDataset()
         self.pitcher_dataset = PitcherDataset()
-        self.context_dataset = ContextDataset("data/context_2025_full.parquet")
-        self.pitch_sequence_dataset = PitchSequenceDataset("data/pitch_sequence_2025.parquet", sample=self.sample)
-        
+        self.context_dataset = ContextDataset()
+        self.pitch_sequence_dataset = PitchSequenceDataset(sample=self.sample)
+
         # Initialize model_params 
-        self.pitch_seq_model_params = ModelComponents(learning_rate=pitch_sequence_pipeline_components.learning_rate_pitch_sequence, dropout=pitch_sequence_pipeline_components.dropout_pitch_sequence, dataset=self.pitch_sequence_dataset, hidden_dim=256, embed_dim=128)
-        
+        self.pitch_seq_model_params = ModelComponents(
+            learning_rate=pitch_sequence_pipeline_components.learning_rate_pitch_sequence,
+            dropout=pitch_sequence_pipeline_components.dropout_pitch_sequence, dataset=self.pitch_sequence_dataset,
+            hidden_dim=256, embed_dim=128)
+
         # Initialize all encoders
         self.pitch_sequence_encoder = PitchSequenceEncoder(self.pitch_seq_model_params)
-        
+
         # Custom Dataclass for the Trainer
         components = TrainerComponents(
             hitter_embeds=self.hitter_dataset,
